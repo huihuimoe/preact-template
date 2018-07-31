6c00a169f990ffed96fd18f3851e60ea07708003
huihuimoe <0w0@loli.pet>
2018-07-31 12:05:51 +0800

    fix(babel): deprecated preset-es2015 and preset-stage-0

--- webpack.config.js
+++ webpack.config.js
@@ -61,12 +61,35 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           'presets': [
-            ['@babel/preset-es2015', { 'loose': true }],
-            ['@babel/preset-stage-0', { 'decoratorsLegacy': true }]
+            ['@babel/preset-env', { 'loose': true }]
           ],
           'plugins': [
-            ['@babel/plugin-transform-react-jsx', { 'pragma': 'h' }],
+            // Stage 0
+            '@babel/plugin-proposal-function-bind',
+
+            // Stage 1
+            '@babel/plugin-proposal-export-default-from',
+            '@babel/plugin-proposal-logical-assignment-operators',
+            ['@babel/plugin-proposal-optional-chaining', { 'loose': false }],
+            ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
+            ['@babel/plugin-proposal-nullish-coalescing-operator', { 'loose': false }],
+            '@babel/plugin-proposal-do-expressions',
+
+            // Stage 2
+            ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+            '@babel/plugin-proposal-function-sent',
+            '@babel/plugin-proposal-export-namespace-from',
+            '@babel/plugin-proposal-numeric-separator',
+            '@babel/plugin-proposal-throw-expressions',
+
+            // Stage 3
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-syntax-import-meta',
             ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+            '@babel/plugin-proposal-json-strings',
+
+            // JSX
+            ['@babel/plugin-transform-react-jsx', { 'pragma': 'h' }],
             ...(() => ISDEV ? [] : [
               'transform-react-remove-prop-types'
             ])()
